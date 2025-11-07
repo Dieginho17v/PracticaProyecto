8d0d6b4fbc868c56c4709af8fd76a20645f9f388
Dieginho17v <149740312+Dieginho17v@users.noreply.github.com>
2025-11-07 10:09:17 -0500

    Update ValoracionDAO.py

--- dao/ValoracionDAO.py
+++ dao/ValoracionDAO.py
@@ -1,18 +1,63 @@
 from Datos import get_conn
 
 class ValoracionDAO:
+
     @staticmethod
     def insertarValoracion(data):
         with get_conn() as conn:
-            cur = conn.execute(
-                "INSERT INTO valoracion(satisfaccion, comentario, pedido_id_pedido) VALUES (?,?,?)",
-                (data["satisfaccion"], data.get("comentario"), data["pedido_id_pedido"])
-            )
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO valoracion (satisfaccion, comentario, pedido_id_pedido)
+                VALUES (?, ?, ?)
+            """, (
+                data.get("satisfaccion"),
+                data.get("comentario"),
+                data.get("pedido_id_pedido")
+            ))
             conn.commit()
-            return cur.lastrowid
+            return cursor.lastrowid
 
     @staticmethod
     def consultarValoraciones():
         with get_conn() as conn:
-            filas = conn.execute("SELECT * FROM valoracion").fetchall()
-            return [dict(f) for f in filas]
+            cursor = conn.execute("SELECT * FROM valoracion")
+            return [dict(row) for row in cursor.fetchall()]
+
+    @staticmethod
+    def consultarValoracion(id_valoracion):
+        with get_conn() as conn:
+            cursor = conn.execute("SELECT * FROM valoracion WHERE id_valoracion=?", (id_valoracion,))
+            row = cursor.fetchone()
+            return dict(row) if row else None
+
+    @staticmethod
+    def actualizarValoracion(id_valoracion, data):
+        with get_conn() as conn:
+            conn.execute("""
+                UPDATE valoracion
+                SET satisfaccion=?, comentario=?, pedido_id_pedido=?
+                WHERE id_valoracion=?
+            """, (
+                data.get("satisfaccion"),
+                data.get("comentario"),
+                data.get("pedido_id_pedido"),
+                id_valoracion
+            ))
+            conn.commit()
+
+    @staticmethod
+    def actualizarValoracionParcial(id_valoracion, data):
+        campos, valores = [], []
+        for k, v in data.items():
+            campos.append(f"{k}=?")
+            valores.append(v)
+        valores.append(id_valoracion)
+        with get_conn() as conn:
+            conn.execute(f"UPDATE valoracion SET {', '.join(campos)} WHERE id_valoracion=?", valores)
+            conn.commit()
+
+    @staticmethod
+    def eliminarValoracion(id_valoracion):
+        with get_conn() as conn:
+            conn.execute("DELETE FROM valoracion WHERE id_valoracion=?", (id_valoracion,))
+            conn.commit()
