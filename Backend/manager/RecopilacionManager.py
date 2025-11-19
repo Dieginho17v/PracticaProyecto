f6aa5882d1c47026525c62a31c8ca5805550d34a
Mario2305040 <camilobarbo470@gmail.com>
2025-11-18 21:04:29 -0500

    Update RecopilacionManager.py

--- Backend/manager/RecopilacionManager.py
+++ Backend/manager/RecopilacionManager.py
@@ -1,10 +1,52 @@
+# manager/RecopilacionManager.py
+
 from dao.RecopilacionDAO import RecopilacionDAO
 from dao.CancionRecopilacionDAO import CancionRecopilacionDAO
 
 class RecopilacionManager:
-    def crearRecopilacion(self, data):
-        return RecopilacionDAO.insertarRecopilacion(data)
 
-    def asociarCanciones(self, id_recopilacion, canciones):
-        for c in canciones:
-            CancionRecopilacionDAO.asociarCancion(id_recopilacion, c)
+    # -------------------------
+    #     RECOPILACIONES
+    # -------------------------
+    @staticmethod
+    def crearRecopilacion(data, imagen=None):
+        return RecopilacionDAO.insertarRecopilacion(data, imagen)
+
+    @staticmethod
+    def obtenerRecopilaciones():
+        return RecopilacionDAO.consultarRecopilaciones()
+
+    @staticmethod
+    def obtenerRecopilacion(id_recopilacion):
+        return RecopilacionDAO.consultarRecopilacion(id_recopilacion)
+
+    @staticmethod
+    def obtenerCaratula(id_recopilacion):
+        return RecopilacionDAO.obtenerCaratula(id_recopilacion)
+
+    @staticmethod
+    def actualizarRecopilacion(id_recopilacion, data, imagen=None):
+        return RecopilacionDAO.actualizarRecopilacion(id_recopilacion, data, imagen)
+
+    @staticmethod
+    def actualizarRecopilacionParcial(id_recopilacion, data, imagen=None):
+        return RecopilacionDAO.actualizarRecopilacionParcial(id_recopilacion, data, imagen)
+
+    @staticmethod
+    def eliminarRecopilacion(id_recopilacion):
+        return RecopilacionDAO.eliminarRecopilacion(id_recopilacion)
+
+    # -------------------------
+    #      CANCIONES–RECO
+    # -------------------------
+    @staticmethod
+    def agregarCancionARecopilacion(id_cancion, id_recopilacion):
+        return CancionRecopilacionDAO.agregarCancionARecopilacion(id_cancion, id_recopilacion)
+
+    @staticmethod
+    def obtenerCancionesDeRecopilacion(id_recopilacion):
+        return CancionRecopilacionDAO.obtenerCancionesDeRecopilacion(id_recopilacion)
+
+    @staticmethod
+    def eliminarCancionDeRecopilacion(id_cancion, id_recopilacion):
+        return CancionRecopilacionDAO.eliminarCancionDeRecopilacion(id_cancion, id_recopilacion)
