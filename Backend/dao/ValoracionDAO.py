48e954461288dad38ef931912f158e093d83ca6b
Mario2305040 <camilobarbo470@gmail.com>
2025-11-18 20:55:31 -0500

    Update ValoracionDAO.py

--- Backend/dao/ValoracionDAO.py
+++ Backend/dao/ValoracionDAO.py
@@ -1,63 +1,73 @@
-from Datos import get_conn
+# dao/ValoracionDAO.py
+from Datos import obtener_conexion
 
 class ValoracionDAO:
 
     @staticmethod
     def insertarValoracion(data):
-        with get_conn() as conn:
-            cursor = conn.cursor()
-            cursor.execute("""
-                INSERT INTO valoracion (satisfaccion, comentario, pedido_id_pedido)
-                VALUES (?, ?, ?)
-            """, (
-                data.get("satisfaccion"),
-                data.get("comentario"),
-                data.get("pedido_id_pedido")
-            ))
-            conn.commit()
-            return cursor.lastrowid
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO Valoracion(puntuacion, comentario, id_producto, id_usuario)
+            VALUES (?, ?, ?, ?)
+        """, (data["puntuacion"], data.get("comentario"), data["id_producto"], data["id_usuario"]))
+        conn.commit()
+        return cursor.lastrowid
 
     @staticmethod
     def consultarValoraciones():
-        with get_conn() as conn:
-            cursor = conn.execute("SELECT * FROM valoracion")
-            return [dict(row) for row in cursor.fetchall()]
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Valoracion")
+        return cursor.fetchall()
 
     @staticmethod
     def consultarValoracion(id_valoracion):
-        with get_conn() as conn:
-            cursor = conn.execute("SELECT * FROM valoracion WHERE id_valoracion=?", (id_valoracion,))
-            row = cursor.fetchone()
-            return dict(row) if row else None
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Valoracion WHERE id_valoracion=?", (id_valoracion,))
+        return cursor.fetchone()
 
     @staticmethod
     def actualizarValoracion(id_valoracion, data):
-        with get_conn() as conn:
-            conn.execute("""
-                UPDATE valoracion
-                SET satisfaccion=?, comentario=?, pedido_id_pedido=?
-                WHERE id_valoracion=?
-            """, (
-                data.get("satisfaccion"),
-                data.get("comentario"),
-                data.get("pedido_id_pedido"),
-                id_valoracion
-            ))
-            conn.commit()
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute("""
+            UPDATE Valoracion
+            SET puntuacion=?, comentario=?, id_producto=?, id_usuario=?
+            WHERE id_valoracion=?
+        """, (
+            data["puntuacion"], data.get("comentario"),
+            data["id_producto"], data["id_usuario"],
+            id_valoracion
+        ))
+        conn.commit()
+        return cursor.rowcount
 
     @staticmethod
     def actualizarValoracionParcial(id_valoracion, data):
-        campos, valores = [], []
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        campos = []
+        valores = []
+
         for k, v in data.items():
             campos.append(f"{k}=?")
             valores.append(v)
+
         valores.append(id_valoracion)
-        with get_conn() as conn:
-            conn.execute(f"UPDATE valoracion SET {', '.join(campos)} WHERE id_valoracion=?", valores)
-            conn.commit()
+
+        cursor.execute(
+            f"UPDATE Valoracion SET {', '.join(campos)} WHERE id_valoracion=?",
+            valores
+        )
+        conn.commit()
+        return cursor.rowcount
 
     @staticmethod
     def eliminarValoracion(id_valoracion):
-        with get_conn() as conn:
-            conn.execute("DELETE FROM valoracion WHERE id_valoracion=?", (id_valoracion,))
-            conn.commit()
+        conn = obtener_conexion()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Valoracion WHERE id_valoracion=?", (id_valoracion,))
+        conn.commit()
+        return cursor.rowcount
